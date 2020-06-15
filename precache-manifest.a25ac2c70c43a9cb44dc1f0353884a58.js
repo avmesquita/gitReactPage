1b8fb84a471af7809a356cdb533331d02a0c846b
avmesquita <avmesquita@gmail.com>
2020-06-15 14:34:32 +0100

    Updates

---
+++ precache-manifest.a25ac2c70c43a9cb44dc1f0353884a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dafcfec064c0fb0f363b6055197ddd8",
+    "url": "/gitReactPage/index.html"
+  },
+  {
+    "revision": "2437cd8c0646fced35eb",
+    "url": "/gitReactPage/static/css/main.956eeac4.chunk.css"
+  },
+  {
+    "revision": "f90c3445e92becccad50",
+    "url": "/gitReactPage/static/js/2.b6606f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitReactPage/static/js/2.b6606f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2437cd8c0646fced35eb",
+    "url": "/gitReactPage/static/js/main.380dbdfe.chunk.js"
+  },
+  {
+    "revision": "0130a07ea18e248d8df4",
+    "url": "/gitReactPage/static/js/runtime-main.7d442ba4.js"
+  },
+  {
+    "revision": "8286644c949fd34bed51e67cf337b695",
+    "url": "/gitReactPage/static/media/logo.8286644c.svg"
+  }
+]);
